c71fb68d0d002ae9eee400b756465dab39432fd5
Nasse753 <nasse753@gmail.com>
2023-10-25 16:02:39 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,28 +1,36 @@
 #include "lists.h"
 
 /**
-* main - check the code
-* Return: Always 0.
-*/
-int main(void)
-{
-listint_t *head;
-listint_t *new;
-listint_t hello = {8, NULL};
-size_t n;
 
-head = &hello;
-new = malloc(sizeof(listint_t));
-if (new == NULL)
+ * print_listint - prints all the elements of a linked list
+ 
+ * @h: linked list of type listint_t to print
+
+ *
+
+ * Return: number of nodes
+
+*/
+
+size_t print_listint(const listint_t *h)
+
 {
-printf("Error\n");
-return (1);
-}
-new->n = 9;
-new->next = head;
-head = new;
-n = print_listint(head);
-printf("-> %lu elements\n", n);
-free(new);
-return (0);
+
+	size_t num = 0;
+
+	while (h)
+
+	{
+
+		printf("%d\n", h->n);
+
+		num++;
+
+	
+		h = h->next;
+
+	}
+
+	return (num);
+
 }
