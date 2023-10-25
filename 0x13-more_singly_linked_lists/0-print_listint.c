fd295337f8d43b225a9d4d15ff66f07ce65d05be
Nasse753 <nasse753@gmail.com>
2023-10-24 18:20:42 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t node_count = 0; // Initialize the count of nodes to 0
+	size_t node_count = 0; // Initialize the count of nodes to 0
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n); // Print the value of the current node
-        h = h->next; // Move to the next node
-        node_count++; // Increment the node count
-    }
+	while (h != NULL)
+	{
+	printf("%d\n", h->n); // Print the value of the current node
+	h = h->next; // Move to the next nod
+	node_count++; // Increment the node count
+	}
 
-    return node_count; // Return the total number of nodes
+	return node_count; // Return the total number of nodes
 }
